9204dfa6c62a19cea3f704030d42afca5f610d94
Abhi Arya <abhigyaa@uci.edu>
2024-07-08 03:23:49 -0700

    add launch deets

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
+"""
+
+Pip Setup for Tuna
+
+"""
+
 from setuptools import setup, find_packages
 
 setup(
     name='tuna',
     version='0.1',
     py_modules=['cli'],
-    requires=[
-        'halo', 
+    packages=find_packages(),
+    install_requires=[
         'inquirer',
         'requests',
+        'halo',
         'jupyterlab',
-        'nbformat'
+        'nbformat',
+        'scikit-learn',
+        'tabulate',
+        'paramiko'
     ],
     entry_points={
         'console_scripts': [
